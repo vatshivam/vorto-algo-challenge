8273b6b9bc8ae42406c08218a1deb23be7c7ba97
Shivam Vats <shivam88y@yahoo.com>
2024-02-26 00:24:15 -0700

    added comments

--- mySubmission.py
+++ mySubmission.py
@@ -3,100 +3,110 @@ from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 from scipy.spatial import distance_matrix 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("path", help="enter path to problem")
-args = parser.parse_args()
-path = args.path
+def extract_pickups_dropoffs(path):
+    
+    """ Reads Problem txt file """
+    
+    pickups = []
+    dropoffs = []
+    f = open(path, "r")
+    f.readline()
+    for x in f:
+        pickup,drop = (x.split()[1:])
+        pickup = list(eval(pickup))
+        drop = list(eval(drop))
+        pickups.append(pickup)
+        dropoffs.append(drop)
+    f.close()
+    return pickups,dropoffs
 
-pickups = []
-drops = []
-f = open(path, "r")
-f.readline()
-for x in f:
-    pickup,drop = (x.split()[1:])
-    pickup = list(eval(pickup))
-    drop = list(eval(drop))
-    pickups.append(pickup)
-    drops.append(drop)
-f.close()
-
-all_points = [[0,0]] + pickups + drops    
-dist_matrix = distance_matrix(all_points, all_points, p=2).round().astype(int).tolist()
-pickup_deliveries = [[i,i+len(pickups)] for i in range(1,len(pickups)+1)]
-
-# # In each pair of pickup_deliveries, the first entry is the pickup location index and the second is dropoff index
-def create_data_model():
-    """Stores the data for the problem."""
+def create_data_model(pickup_deliveries,distance_matrix):
+    
+    """ Stores the data for the problem. """
+    
     data = {}
-    data["demands"] = [0]+[2]*(len(pickup_deliveries))+[-2]*(len(pickup_deliveries))
-    data["distance_matrix"] = dist_matrix
+    data["distance_matrix"] = distance_matrix
     data["pickups_deliveries"] = pickup_deliveries
-    data["num_vehicles"] = len(pickup_deliveries)
-    data["vehicle_capacities"] = [2]*len(pickup_deliveries)
     data["depot"] = 0
+    
+    # initializing number of drivers equal to the number of dropoffs
+    data["num_vehicles"] = len(pickup_deliveries)
+    
+    # Setting demand for pickup locations equal to 2 and -2 for dropoffs.
+    # This forces the driver to return to depot after every delivery as the vehicle capacity becomes 0 after a delivery is complete
+    # Note: Value "2" could be replaced by an arbitrary number since the value doesn't have any significance
+    data["vehicle_capacities"] = [2]*len(pickup_deliveries)
+    data["demands"] = [0]+[2]*(len(pickup_deliveries))+[-2]*(len(pickup_deliveries))
     return data
 
 def print_solution(data, manager, routing, solution):
+    
     """Prints solution on console."""
-    # print(f"Objective: {solution.ObjectiveValue()}")
-    # total_distance = 0
+
     for vehicle_id in range(data["num_vehicles"]):
-        # print("vehivle_id",vehicle_id)
         index = routing.Start(vehicle_id)
-        # plan_output = f"Route for vehicle {vehicle_id}:\n"
         route_distance = 0
-        nodes_out = []
+        nodes_output = []
         while not routing.IsEnd(index):
             node = manager.IndexToNode(index)
+            
+            # appending the load_id to the output array when the delivery is complete.
+            # Delivery is considered to be complete if the load_id is greater than the number of pickup-dropoff pairs.
+            # This is because, pickups and dropoffs are ordered like [depot, pickup1, pickup2, pickup_n, dropoff1, dropoff2, dropoffn]
             if node>len(pickup_deliveries):
-                nodes_out.append(node-len(pickup_deliveries))
-            # plan_output += f" {node} -> "
+                nodes_output.append(node-len(pickup_deliveries))
+            
+            # moving the pointer to next node
             previous_index = index
             index = solution.Value(routing.NextVar(index))
+            
+            # adding transit distance to total route distance 
             route_distance += routing.GetArcCostForVehicle(
                 previous_index, index, vehicle_id
             )
-            # print("distance travelled",route_distance)
         if route_distance == 0:
             continue
-        # route_distance -= 500
-        # plan_output += f"{manager.IndexToNode(index)}\n"
-        # plan_output += f"Distance of the route: {route_distance}m\n"
-        # print(plan_output)
-        # total_distance += route_distance
-        if len(nodes_out)>=1:
-            print(nodes_out)
-    # print(f"Total Distance of all routes: {total_distance}m")
+        if len(nodes_output)>=1:
+            print("["+",".join(str(i) for i in nodes_output)+"]")
+            
+def find_solution(pickup_deliveries,dist_matrix,max_distance):
     
-def main():
-    """Entry point of the program."""
+    """Finding solution for the routing problem"""
+    
     # Instantiate the data problem.
-    data = create_data_model()
+    data = create_data_model(pickup_deliveries,dist_matrix)
+    
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(
         len(data["distance_matrix"]),
         data["num_vehicles"],
         data["depot"]
     )
+    
     # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
     # Define cost of each arc.
     def distance_callback(from_index, to_index):
-        """Returns the manhattan distance between the two nodes."""
+        
+        """ Returns the manhattan distance between the two nodes. """
+        
         # Convert from routing variable Index to distance matrix NodeIndex.
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
         return data["distance_matrix"][from_node][to_node]
 
     def demand_callback(from_index):
-        """Returns the demand of the node."""
+        
+        """ Returns the demand of the node. """
+        
         # Convert from routing variable Index to demands NodeIndex.
         from_node = manager.IndexToNode(from_index)
         return data["demands"][from_node]
     
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    
     #adding fixed cost of 500 for each driver
     routing.SetFixedCostOfAllVehicles(500)
 
@@ -109,12 +119,13 @@ def main():
         True,  # start cumul to zero
         "Capacity",
     )
+    
     # Add Distance constraint.
     dimension_name = "Distance"
     routing.AddDimension(
         transit_callback_index,
         0,  # no slack
-        719,  # vehicle maximum travel distance
+        max_distance,  # vehicle maximum travel distance
         False,  #  Setting the value of fix_start_cumul_to_zero to true will force the "cumul" variable of the start node of all vehicles to be equal to 0
         dimension_name,
     )
@@ -124,16 +135,16 @@ def main():
 
     # Define Transportation Requests.
     for request in data["pickups_deliveries"]:
-        depot_index = manager.NodeToIndex(data["depot"])
         pickup_index = manager.NodeToIndex(request[0])
-        # print("pikcup index:",pickup_index)
         delivery_index = manager.NodeToIndex(request[1])
-        # print("delivery index",delivery_index)
         routing.AddPickupAndDelivery(pickup_index, delivery_index)
-        # routing.AddPickupAndDelivery(depot_index, depot_index)
+        
+        # Ensuring that same vehicle is used for the pickup-dropoff pair
         routing.solver().Add(
             routing.VehicleVar(pickup_index) == routing.VehicleVar(delivery_index)
         )
+        
+        # Cumulative distance at pickup should be smaller that dropoff
         routing.solver().Add(
             distance_dimension.CumulVar(pickup_index)
             <= distance_dimension.CumulVar(delivery_index)
@@ -146,9 +157,30 @@ def main():
     )
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
+    
     # Print solution on console.
     if solution:
-        # print_solution_new(data, manager, routing, solution)
         print_solution(data, manager, routing, solution)
-if __name__ == "__main__":
-    main()
+
+# Parsing input problem path
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="enter path to problem")
+args = parser.parse_args()
+path = args.path
+
+# Extract pickup and dropoff coordinates from txt file
+pickups,drops = extract_pickups_dropoffs(path)
+
+# List of all points including depot.
+all_points = [[0,0]] + pickups + drops
+
+# calculate pairwise distances between all the pickup and dropoff points    
+dist_matrix = distance_matrix(all_points, all_points, p=2).round().astype(int).tolist()
+
+# In Each pair of pickup_deliveries, the first entry is the pickup location index and the second is dropoff index with respect to "all_points"
+pickup_deliveries = [[i,i+len(pickups)] for i in range(1,len(pickups)+1)]
+    
+# Maximum dsitance for each driver
+max_distance = 12*60 - 1
+
+find_solution(pickup_deliveries,dist_matrix,max_distance)
